f472a32ca253a69edc5d7505d521693c04dcf540
Biljana Zivkovic <77129646+biljanazivkovic@users.noreply.github.com>
2021-01-08 16:01:56 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 10.99;
+            int y = 15;
+            Console.WriteLine("Max: " + Math.Max(x, y));
+            Console.WriteLine("Min: " + Math.Min(x, y));
+            Console.WriteLine("Sqrt(x): " + Math.Sqrt(x));
+            Console.WriteLine("Abs(x): " + Math.Abs(x));
+            Console.WriteLine("Round(x): " + Math.Round(x));
+        }
+    }
+}
